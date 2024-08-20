ddbca844b70f849d4cb92905603a7c3574c82746
Emerson <emerx25@gmail.com>
2024-08-20 19:34:36 -0400

    Added popup info.

--- app.py
+++ app.py
@@ -11,15 +11,15 @@ metro_df = pd.read_csv("src/DC_Metro_Stations.csv")
 
 # Add markers for historical/important places
 for lt, ln, place in zip(historical_df["Latitude"], historical_df["Longitude"], historical_df["Name"]):
-    map.add_child(fl.Marker(location=[lt, ln], popup=place))
+    map.add_child(fl.Marker(location=[lt, ln], popup=place, icon=fl.Icon(color="green")))
 
 # Add markers for historical/important places
 for lt, ln, place in zip(universities_df["Latitude"], universities_df["Longitude"], universities_df["Name"]):
-    map.add_child(fl.Marker(location=[lt, ln], popup=place))
+    map.add_child(fl.Marker(location=[lt, ln], popup=place, icon=fl.Icon(color="red")))
 
 # Add markers for historical/important places
 for lt, ln, place in zip(metro_df["Latitude"], metro_df["Longitude"], metro_df["Name"]):
-    map.add_child(fl.Marker(location=[lt, ln], popup=place))
+    map.add_child(fl.Marker(location=[lt, ln], popup=place, icon=fl.Icon(color="blue")))
 
 # Save the map to an HTML file
 map.save("dc_map.html")
