b99870f488dfb780deb9149db2ddfdfb161a4ac5
Emerson <emerx25@gmail.com>
2024-08-20 20:06:19 -0400

    Added layers.

--- app.py
+++ app.py
@@ -9,17 +9,29 @@ historical_df = pd.read_csv("src/DC_Historical_Important_Places.csv")
 universities_df = pd.read_csv("src/DC_Universities.csv")
 metro_df = pd.read_csv("src/DC_Metro_Stations.csv")
 
+hist_group =fl.FeatureGroup(name="Important Places")
+uni_group = fl.FeatureGroup(name="Universities")
+metro_group = fl.FeatureGroup(name="Metro Starions")
+
 # Add markers for historical/important places
 for lt, ln, place in zip(historical_df["Latitude"], historical_df["Longitude"], historical_df["Name"]):
-    map.add_child(fl.Marker(location=[lt, ln], popup=place, icon=fl.Icon(color="green")))
+    hist_group.add_child(fl.Marker(location=[lt, ln], popup=place, icon=fl.Icon(color="green")))
+
+map.add_child(hist_group)
 
 # Add markers for historical/important places
 for lt, ln, place in zip(universities_df["Latitude"], universities_df["Longitude"], universities_df["Name"]):
-    map.add_child(fl.Marker(location=[lt, ln], popup=place, icon=fl.Icon(color="red")))
+    uni_group.add_child(fl.Marker(location=[lt, ln], popup=place, icon=fl.Icon(color="red")))
+
+map.add_child(uni_group)
 
 # Add markers for historical/important places
 for lt, ln, place in zip(metro_df["Latitude"], metro_df["Longitude"], metro_df["Name"]):
-    map.add_child(fl.Marker(location=[lt, ln], popup=place, icon=fl.Icon(color="blue")))
+    metro_group.add_child(fl.Marker(location=[lt, ln], popup=place, icon=fl.Icon(color="blue")))
+
+map.add_child(metro_group)
+
+map.add_child(fl.LayerControl())
 
 # Save the map to an HTML file
 map.save("dc_map.html")
