4c748ccc3a4c1325d6fc91b1762fca8c390d2044
Emerson <emerx25@gmail.com>
2024-08-20 20:59:47 -0400

    Refactoring

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
 import folium as fl
 import pandas as pd
 
+def add_markers(df, group, color):
+    for lt, ln, place in zip(df["Latitude"], df["Longitude"], df["Name"]):
+        group.add_child(fl.Marker(location=[lt, ln], popup=place, icon=fl.Icon(color=color)))
+    return group
+
+
 # Initialize the map centered at a given location
 map = fl.Map(location=[38.914031, -77.037238], zoom_start=12)
 
@@ -13,23 +19,9 @@ hist_group =fl.FeatureGroup(name="Important Places")
 uni_group = fl.FeatureGroup(name="Universities")
 metro_group = fl.FeatureGroup(name="Metro Starions")
 
-# Add markers for historical/important places
-for lt, ln, place in zip(historical_df["Latitude"], historical_df["Longitude"], historical_df["Name"]):
-    hist_group.add_child(fl.Marker(location=[lt, ln], popup=place, icon=fl.Icon(color="green")))
-
-map.add_child(hist_group)
-
-# Add markers for historical/important places
-for lt, ln, place in zip(universities_df["Latitude"], universities_df["Longitude"], universities_df["Name"]):
-    uni_group.add_child(fl.Marker(location=[lt, ln], popup=place, icon=fl.Icon(color="red")))
-
-map.add_child(uni_group)
-
-# Add markers for historical/important places
-for lt, ln, place in zip(metro_df["Latitude"], metro_df["Longitude"], metro_df["Name"]):
-    metro_group.add_child(fl.Marker(location=[lt, ln], popup=place, icon=fl.Icon(color="blue")))
-
-map.add_child(metro_group)
+map.add_child(add_markers(historical_df, hist_group, "green"))
+map.add_child(add_markers(universities_df, uni_group, "red"))
+map.add_child(add_markers(metro_df, metro_group, "blue"))
 
 map.add_child(fl.LayerControl())
 
